6f47027e0e5533b539688a8949ca5c83752cd5d7
Kartik Shah <shahkartikr@gmail.com>
2011-09-29 08:11:02 -0500

    Added documentation to functions

--- src/clj_ml_class/classifier.clj
+++ src/clj_ml_class/classifier.clj
@@ -1,31 +1,42 @@
-(defn tokenize-doc [doc]
+(ns clj_ml_class.classifier)
+
+(defn tokenize-doc
+  "Tokenizes an article (document) by word"
+  [doc]
   (re-seq #"\w+" doc))
 
-(defn length-filter [n lst]
+(defn length-filter
+  "Filters features (word) by length"
+  [n lst]
   (filter #(> (count %) n)
           lst))
 
-(defn tokenize-features [docs category]
+(defn tokenize-features
+  "Tokenizes all docs (articles) by category e.g. :sports :arts"
+  [docs category]
   (flatten (map tokenize-doc (category docs))))
 
-(defn calc-freq [docs category]
-  (frequencies (length-filter 2 (tokenize-features docs category))))
-
 (defn train
+  "Trains from supplied documents and category mapping."
   [docs category]
   (->> (tokenize-features docs category)
        (length-filter 2)
        (frequencies)))
 
-(defn update-map-fn [category]
+(defn update-map-fn
+  "Update Map Utility function to insert categories in parsed data. e.g {\"feature\" {:sports 1, :arts 2 }}"
+  [category]
   (fn [m entrykey] (update-in m [entrykey] #(hash-map category %))))
 
-(defn categorize [train-data categories]
+(defn categorize
+  "Categorizes train data"
+  [train-data categories]
   (let [entrykeys (keys train-data)]
     (reduce (update-map-fn categories) train-data entrykeys)
     ))
 
 (defn train-from-data
+  "Trains from data"
   [docs]
   (apply merge-with
          conj
@@ -35,16 +46,19 @@
           (keys docs))))
 
 (defn category-count
+  "Calculates category count"
   [knowledge category]
   (reduce + 0 (filter #(not (nil? %)) (map category (vals knowledge)))))
 
 (def select-values (comp vals select-keys))
 
 (defn feature-count
+  "Calculates feature count"
   [knowledge feature category]
    (apply category (select-values knowledge [feature])))
 
 (defn feature-prob
+  "Calculates given feature probablity"
   [knowledge feature category]
   (if (= category-count 0)
       0
